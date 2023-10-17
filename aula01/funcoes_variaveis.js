2921ec11af69db7e93069944c24d11dd4ccf220f
orrico13 <gabriellorrico@hotmail.com>
2023-10-16 22:30:08 -0300

    Primeira Aula de Javascript

---
+++ aula01/funcoes_variaveis.js
@@ -0,0 +1,29 @@
+var variavel1 = "olá";
+let variavel2 = 3;
+const variavel3 = 8; 
+
+console.log(
+    "Olá mundo!"
+);
+
+console.log (variavel1 + "mundo");
+console.log (variavel1, "mundo");
+console.log (variavel1+ "Turminha do"+ variavel2+ "º ano");
+console.log (variavel2 + variavel3);
+console.log (typeof + variavel1);
+console.log (typeof + variavel2);
+variavel1 = 4;
+console.log (typeof variavel1);
+
+const pessoa = {
+    nome: "Gabriel",
+    signo: "Sagitário",
+    idade: 22
+}
+console.log(pessoa)
+console.log(
+    "Nome:", pessoa.nome,
+    "\nIdade:", pessoa.idade
+)
+let cachorros = ["Bug", "Zeus", "Zaratrusta"]
+console.log(cachorros[1])
